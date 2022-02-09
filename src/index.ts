fbb6498581231aa3643254c344d6d689deb57476
Jochen Wikenhauser <noveb@gmx.de>
2022-02-09 22:35:13 +0100

    :sparkles: add stuff for 'replay' feature

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as bodyParser from 'body-parser';
 import express from 'express';
 import morgan from 'morgan';
 import type { Request, Response } from 'express';
+// import path from 'node:path';
 import logger from './logger';
 
 import {
@@ -10,6 +11,7 @@ import {
   genericErrorHandler,
 } from './handlers';
 import gameController from './game';
+// import replayController from './replay';
 import { db } from './database';
 
 const app = express();
@@ -18,8 +20,12 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+// app.set('view engine', 'pug');
+// app.use(express.static(path.join(`${__dirname}/src/views`)));
+// app.set('views', './src/views');
 
 app.use(gameController.router);
+// app.use('/replay', replayController.router);
 app.get('/health', (req: Request, res: Response) => {
   if (db.readyState !== 1) {
     return res.status(500).json({
